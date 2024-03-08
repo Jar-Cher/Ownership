f7d78e572f4bc3c64728a6e917b5a7a2e2147aec
Jaroslav Chernyshev <31724693+Jar-Cher@users.noreply.github.com>
2024-03-08 21:10:37 +0300

    + Path parsing; "for" refactor

--- src/main/java/org/example/FileChange.java
+++ src/main/java/org/example/FileChange.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -16,21 +17,23 @@ public class FileChange {
         int j = -1;
         boolean upcomingChanges = false;
         // There goes "git diff" parser, splitting "gitDiff" into separate file changes
-        for (int i = 0; i < gitDiff.size(); i++) {
-            if (Pattern.matches("^diff --git.*", gitDiff.get(i))) {
+        for (String s : gitDiff) {
+            if (Pattern.matches("^diff --git.*", s)) {
                 upcomingChanges = false;
+                String path = s.split(" ")[2];
+                fileLocation = Paths.get((String) path.subSequence(2, path.length()));
             }
 
             // Deletion
-            if (Pattern.matches("^\\+\\+\\+ /dev/null$", gitDiff.get(i))) {
+            if (Pattern.matches("^\\+\\+\\+ /dev/null$", s)) {
                 changeType = ChangeType.DELETE;
             }
             // Addition
-            else if (Pattern.matches("^new file mode.+", gitDiff.get(i))) {
+            else if (Pattern.matches("^new file mode.+", s)) {
                 changeType = ChangeType.ADD;
             }
 
-            if (Pattern.matches("^@@ -\\d+,\\d+ \\+\\d+,\\d+ @@.*", gitDiff.get(i))) {
+            if (Pattern.matches("^@@ -\\d+,\\d+ \\+\\d+,\\d+ @@.*", s)) {
                 if (j != -1) {
                     codeChanges.add(codeChange);
                     codeChange = new CodeChange();
@@ -39,11 +42,10 @@ public class FileChange {
                 j++;
             }
 
-            if (Pattern.matches("^\\+.*", gitDiff.get(i)) && upcomingChanges) {
-                codeChange.addition.add(gitDiff.get(i));
-            }
-            else if (Pattern.matches("^-.*", gitDiff.get(i)) && upcomingChanges) {
-                codeChange.deletion.add(gitDiff.get(i));
+            if (Pattern.matches("^\\+.*", s) && upcomingChanges) {
+                codeChange.addition.add(s);
+            } else if (Pattern.matches("^-.*", s) && upcomingChanges) {
+                codeChange.deletion.add(s);
             }
         }
         if (!codeChange.addition.isEmpty() || !codeChange.deletion.isEmpty()) {
--- src/test/java/BasicTest.java
+++ src/test/java/BasicTest.java
@@ -11,8 +11,10 @@ public class BasicTest {
         CommitChange commitChange = new CommitChange(Paths.get("src/test/resources/5.txt"));
         Assertions.assertEquals(commitChange.commitChanges.size(), 2);
         Assertions.assertEquals(commitChange.commitChanges.get(0).codeChanges.size(), 0);
-        Assertions.assertTrue(commitChange.commitChanges.get(0).changeType == ChangeType.ADD);
+        Assertions.assertSame(commitChange.commitChanges.get(0).changeType, ChangeType.ADD);
+        Assertions.assertEquals(commitChange.commitChanges.get(0).fileLocation.toString(), "input\\Test.txt");
         Assertions.assertEquals(commitChange.commitChanges.get(1).codeChanges.get(0).deletion.size(), 9);
-        Assertions.assertTrue(commitChange.commitChanges.get(1).changeType == ChangeType.DELETE);
+        Assertions.assertSame(commitChange.commitChanges.get(1).changeType, ChangeType.DELETE);
+        Assertions.assertEquals(commitChange.commitChanges.get(1).fileLocation.toString(), "input\\top20.txt");
     }
 }
